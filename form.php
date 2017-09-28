8fc41b779012c58819fc3abca06dc30f23e0fc6d
nicolauslawe <nicolauslawe@gmail.com>
2017-09-28 23:42:09 +0700

    Level 1
    Validating the Form Values

--- form.php
+++ form.php
@@ -1,12 +1,30 @@
 <?php
 if(isset($_POST['submit']))
-{ 
+{
+	$name = array("nandadwi", "nicolas", "bestario", "bonggal");
+	$min = 5;
+	$max = 10;
+	
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	
-	echo $username;
-	echo "<br/>";
-	echo $password;
+	if(strlen($username)<$min)
+	{
+		echo "username harus memiliki panjang 5 atau lebih.";
+	}
+	if(strlen($username)>$max)
+	{
+		echo "username tidak boleh lebih panjang dari 10.";
+	}
+	
+	if(!in_array($username, $name))
+	{
+		echo "<br/>mohon maaf, akses anda gagal!";
+	}
+	else
+	{
+		echo "<br/>selamat datang bous!!";
+	}
 }
 ?>
 
